836407aa081ee7dd14f0f73f38e625d244846162
nana <eyzzz_@naver.com>
2022-01-11 11:25:55 +0900

    TE-search-0111-01

--- ready_table/src/components/units/search/search.presenter.tsx
+++ ready_table/src/components/units/search/search.presenter.tsx
@@ -1,118 +1,163 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {
-  SafeAreaView,
-  Text,
-  StyleSheet,
-  View,
-  FlatList,
-  TextInput
-} from "react-native";
+  SearchBar,
+  SearchBarLine,
+  SearchIcon,
+  SearchPageView,
+  ProductList,
+  Search,
+  Product,
+  ProductImage,
+  ProductName,
+  ProductNameAndPrice,
+  ProductPrice,
+  ProductWrapper,
+  FilterWrapper,
+  Row,
+  FilterFont,
+  FilterToggle,
+  FilterView,
+  Row1,
+  FilterView1,
+  Row3
+} from "./seatch.styles";
+import { ScrollView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
-const App = () => {
-  const [search, setSearch] = useState("");
-  const [filteredDataSource, setFilteredDataSource] = useState([]);
-  const [masterDataSource, setMasterDataSource] = useState([]);
-
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then(response => response.json())
-      .then(responseJson => {
-        setFilteredDataSource(responseJson);
-        setMasterDataSource(responseJson);
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  }, []);
-
-  const searchFilterFunction = text => {
-    // Check if searched text is not blank
-    if (text) {
-      // Inserted text is not blank
-      // Filter the masterDataSource
-      // Update FilteredDataSource
-      const newData = masterDataSource.filter(function (item) {
-        const itemData = item.title
-          ? item.title.toUpperCase()
-          : "".toUpperCase();
-        const textData = text.toUpperCase();
-        return itemData.indexOf(textData) > -1;
-      });
-      setFilteredDataSource(newData);
-      setSearch(text);
-    } else {
-      // Inserted text is blank
-      // Update FilteredDataSource with masterDataSource
-      setFilteredDataSource(masterDataSource);
-      setSearch(text);
-    }
-  };
-
-  const ItemView = ({ item }) => {
-    return (
-      // Flat List Item
-      <Text style={styles.itemStyle} onPress={() => getItem(item)}>
-        {item.id}
-        {"."}
-        {item.title.toUpperCase()}
-      </Text>
-    );
-  };
-
-  const ItemSeparatorView = () => {
-    return (
-      // Flat List Item Separator
-      <View
-        style={{
-          height: 0.5,
-          width: "100%",
-          backgroundColor: "#C8C8C8"
-        }}
-      />
-    );
-  };
-
-  const getItem = item => {
-    // Function for click on an item
-    alert("Id : " + item.id + " Title : " + item.title);
-  };
-
+const SearchUI = (props: any) => {
+  const navigation = useNavigation();
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <TextInput
-          style={styles.textInputStyle}
-          onChangeText={text => searchFilterFunction(text)}
-          value={search}
-          underlineColorAndroid="transparent"
-          placeholder="Search Here"
-        />
-        <FlatList
-          data={filteredDataSource}
-          keyExtractor={(item, index) => index.toString()}
-          ItemSeparatorComponent={ItemSeparatorView}
-          renderItem={ItemView}
-        />
-      </View>
-    </SafeAreaView>
+    <ScrollView
+      onMomentumScrollBegin={props.onLoadMore}
+      showsVerticalScrollIndicator={false}
+      style={{ backgroundColor: "white" }}
+    >
+      <SearchPageView>
+        <Search>
+          <SearchIcon
+            source={require("../../../../public/images/search02.png")}
+          />
+          <SearchBar onChangeText={props.onChangeSearch} />
+        </Search>
+        <SearchBarLine />
+
+        <FilterView1>
+          <Row3>
+            <FilterView>
+              <FilterToggle onPress={props.priceToggleChange}>
+                <FilterFont>가격</FilterFont>
+              </FilterToggle>
+            </FilterView>
+
+            <FilterView>
+              <FilterToggle onPress={props.dateToggleChange}>
+                <FilterFont>날짜</FilterFont>
+              </FilterToggle>
+            </FilterView>
+          </Row3>
+        </FilterView1>
+
+        {props.dateToggle && (
+          <FilterView1>
+            <Row>
+              <FilterWrapper onPress={props.searchAllTime}>
+                <FilterFont>모든 시간</FilterFont>
+              </FilterWrapper>
+
+              <FilterWrapper onPress={props.searchLastDay}>
+                <FilterFont>지난 1일</FilterFont>
+              </FilterWrapper>
+
+              <FilterWrapper onPress={props.searchLastWeek}>
+                <FilterFont>지난 1주</FilterFont>
+              </FilterWrapper>
+            </Row>
+          </FilterView1>
+        )}
+
+        {props.priceToggle && (
+          <FilterView1>
+            <Row>
+              <FilterWrapper onPress={props.searchFiveThousand}>
+                <FilterFont>~5000</FilterFont>
+              </FilterWrapper>
+
+              <FilterWrapper onPress={props.searchOneHundredThousand}>
+                <FilterFont>~10000</FilterFont>
+              </FilterWrapper>
+
+              <FilterWrapper onPress={props.searchOneHundredFiveThousand}>
+                <FilterFont>~15000</FilterFont>
+              </FilterWrapper>
+            </Row>
+          </FilterView1>
+        )}
+
+        <ProductList>
+          {props.dataRes?.map((el: any) => (
+            <ProductWrapper
+              key={el._id}
+              onPress={() => {
+                navigation.navigate("식당 상세보기", {
+                  id: props.onPressDetail(el)
+                });
+              }}
+            >
+              <Product>
+                <ProductImage
+                  source={{
+                    uri: el.images[0]
+                  }}
+                />
+                <ProductNameAndPrice>
+                  <ProductName>
+                    {String(el.name.split("#")[1]).length > 60
+                      ? `${String(el.name.split("#")[1]).substr(0, 61)}...`
+                      : el.name.split("#")[1]}
+                  </ProductName>
+                  <ProductPrice>{`${el.price
+                    .toLocaleString("ko-KR")
+                    .toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원`}</ProductPrice>
+                </ProductNameAndPrice>
+              </Product>
+            </ProductWrapper>
+          ))}
+
+          {!props.dataRes &&
+            props.data?.fetchUseditems.map((el: any) => (
+              <ProductWrapper
+                key={el._id}
+                onPress={() => {
+                  navigation.navigate("식당 상세보기", {
+                    id: props.onPressDetail(el)
+                  });
+                }}
+              >
+                <Product>
+                  <ProductImage
+                    source={{
+                      uri: el.images[0]
+                    }}
+                  />
+                  <ProductNameAndPrice>
+                    <ProductName>
+                      {String(el.name.split("#")[1]).length > 60
+                        ? `${String(el.name.split("#")[1]).substr(0, 61)}...`
+                        : el.name.split("#")[1]}
+                    </ProductName>
+                    <ProductPrice>{`${el.price
+                      .toLocaleString("ko-KR")
+                      .toString()
+                      .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}원`}</ProductPrice>
+                  </ProductNameAndPrice>
+                </Product>
+              </ProductWrapper>
+            ))}
+        </ProductList>
+      </SearchPageView>
+    </ScrollView>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: "white"
-  },
-  itemStyle: {
-    padding: 10
-  },
-  textInputStyle: {
-    height: 40,
-    borderWidth: 1,
-    paddingLeft: 20,
-    margin: 5,
-    borderColor: "#dd4124",
-    backgroundColor: "#FFFFFF"
-  }
-});
-
-export default App;
+export default SearchUI;
--- ready_table/src/components/units/search/seatch.styles.ts
+++ ready_table/src/components/units/search/seatch.styles.ts
@@ -1,16 +1,139 @@
 import styled from "@emotion/native";
 
-export const Searchicon = styled.Image`
-  margin-left: 16px;
+export const SearchPageView = styled.View`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #ffffff;
 `;
-export const TextInputWrapper = styled.TextInput``;
 
-export const SearchWrapper = styled.View`
-  height: 50px;
-  background-color: #f4f4f4;
-  margin-top: 72px;
-  margin-left: 26px;
-  margin-right: 26px;
+export const SearchBar = styled.TextInput`
+  width: 315px;
+  height: 39.74px;
+`;
+
+export const SearchBarLine = styled.View`
+  width: 339px;
+  border-bottom-width: 2px;
+  border-bottom-color: #f1eef6;
+`;
+
+export const SearchIcon = styled.Image`
+  height: 24px;
+  width: 24px;
+`;
+export const Search = styled.View`
+  display: flex;
+  flex-direction: row;
+  width: 339px;
+  height: 39.74px;
+  align-items: center;
+`;
+
+export const ProductList = styled.View`
+  width: 339px;
+  margin-top: 12px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Product = styled.View`
+  width: 339px;
+  height: 119px;
+  display: flex;
+  flex-direction: row;
+  border: 1px solid #dd4124;
+  border-radius: 15px;
+  align-items: center;
+  padding-left: 12px;
+  margin-bottom: 12px;
+`;
+
+export const ProductImage = styled.Image`
+  width: 95px;
+  height: 99px;
+  border-radius: 15px;
+`;
+
+export const ProductNameAndPrice = styled.View`
+  width: 170px;
+  height: 97px;
+  margin-left: 10px;
+`;
+
+export const ProductName = styled.Text`
+  font-family: "NotoSans";
+  color: black;
+`;
+
+export const ProductPrice = styled.Text`
+  font-family: "NotoSans";
+  color: black;
+`;
+
+export const ProductWrapper = styled.TouchableOpacity``;
+
+export const Btn = styled.TouchableOpacity`
+  border: 1px solid black;
+  margin-right: 10px;
+`;
+
+export const FilterWrapper = styled.TouchableOpacity`
+  width: 80px;
+  border: 1px solid black;
+  border: none;
+  border-radius: 10px;
+  background-color: #dd4124;
+  align-items: center;
+  justify-content: center;
+  height: 20px;
+  margin-right: 10px;
+`;
+
+export const FilterFont = styled.Text`
+  font-size: 11px;
+  color: white;
+  font-family: "NotoSans";
+`;
+
+export const Row = styled.View`
+  display: flex;
   flex-direction: row;
   align-items: center;
+  margin-top: 15px;
+`;
+
+export const Row1 = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-top: 15px;
+`;
+
+export const Row3 = styled.View`
+  width: 50%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`;
+
+export const FilterView = styled.View`
+  /* width: 90%; */
+  margin-top: 15px;
+`;
+
+export const FilterView1 = styled.View`
+  width: 90%;
+`;
+
+export const FilterToggle = styled.TouchableOpacity`
+  width: 80px;
+  border: 1px solid black;
+  border: none;
+  border-radius: 10px;
+  background-color: #dd4124;
+  align-items: center;
+  justify-content: center;
+  height: 20px;
 `;
