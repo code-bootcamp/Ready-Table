e9753b5014fa5b2e3a63328f9062d4e5748811f9
heeje0910 <shjs6711@gmail.com>
2022-01-11 12:30:10 +0900

    HJ_src/detail_0111_02

--- ready_table/src/components/units/detail/detail.types.ts
+++ ready_table/src/components/units/detail/detail.types.ts
@@ -1,3 +1,9 @@
+import { GestureResponderEvent } from "react-native";
+
 export interface IdetailProps {
+  onPressCart: (event: GestureResponderEvent) => void;
+  items: any;
   data: any;
 }
+
+export interface IProduct {}
